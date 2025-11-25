9b8c8d9327c7aa1c047d7c7ee7fdf1e94e6d6570
emfermenty <demrar12345@gmail.com>
2025-11-25 11:15:17 +0500

    Убрал маппинг gRPC в доменные модели

--- LogisticService.Infrastructure/Mappers/GrpcShippingMapper.cs
+++ LogisticService.Infrastructure/Mappers/GrpcShippingMapper.cs
@@ -1,6 +1,5 @@
 ﻿using LogisticService.Domain.Models.Shipping;
 using LogisticService.Domain.Models.Shipping.Abstract;
-using ShippingOptimization.Grpc;
 using DomainShippingType = LogisticService.Domain.Enums.ShippingType;
 using GrpcShippingType = ShippingOptimization.Grpc.ShippingType;
 using DomainQuote = LogisticService.Application.DTO.ShippingQuote;
@@ -9,28 +8,7 @@ namespace LogisticService.Infrastructure.External;
 
 public static class GrpcShippingMapper
 {
-    public static Shipping ToDomain(ShippingOptimization.Grpc.ShippingQuote grpc)
-    {
-        var shipping = Create(MapToDomainShippingType(grpc.Type));
-
-        shipping.Cost = grpc.Cost;
-
-        shipping.Duration = grpc.Duration != null
-            ? new TimeSpan(
-                (int)grpc.Duration.Days,
-                (int)grpc.Duration.Hours,
-                (int)grpc.Duration.Minutes,
-                (int)grpc.Duration.Seconds,
-                (int)grpc.Duration.Milliseconds)
-            : TimeSpan.Zero;
-
-        shipping.Weight = grpc.MaxWeight;
-        shipping.Volume = grpc.MaxVolume;
-        shipping.TypeDescription = grpc.Description;
-
-        return shipping;
-    }
-
+    
     public static DomainQuote ToDomainQuote(global::ShippingOptimization.Grpc.ShippingQuote grpcQuote)
     {
         return new DomainQuote
@@ -48,18 +26,6 @@ public static class GrpcShippingMapper
             MaxWeight = grpcQuote.MaxWeight,
             MaxVolume = grpcQuote.MaxVolume,
             Requirements = grpcQuote.Requirements?.ToList() ?? new List<string>()
-        };
-    }
-
-    public static Shipping Create(DomainShippingType type)
-    {
-        return type switch
-        {
-            DomainShippingType.Air   => new AirShipping(),
-            DomainShippingType.Sea   => new SeaShipping(),
-            DomainShippingType.Train => new TrainShipping(),
-            DomainShippingType.Truck => new TruckShipping(),
-            _ => throw new ArgumentOutOfRangeException(nameof(type))
         };
     }
 
